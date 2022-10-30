f24d6909c6ac5a6241781118d8e0ea1e07762295
JakeJR0 <1453194@stu.tmc.ac.uk>
2022-10-30 11:33:07 +0000

    Adding default.sql

---
+++ includes/default.sql
@@ -0,0 +1,10 @@
+INSERT INTO colours(colour_name, colour_hex) 
+VALUES
+('red', '#FF0000'),
+('green', '#00FF00'),
+('blue', '#0000FF'),
+('pink', '#FF00FF'),
+('orange', '#FFA500'),
+('purple', '#800080'),
+('brown', '#A52A2A'),
+('black', '#000000')
